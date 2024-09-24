41a9db1fa57cb62d011bc960e4f67cab976c0277
Daniil50976 <mzxa8sljzx@gmail.com>
2024-09-24 23:11:21 +0500

    100%

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    'https://butcher42.ru/',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => cachedResponse || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
