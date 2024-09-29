5089da2250f3b135177bb3ba676e46333a13a7e8
Daniil50976 <mzxa8sljzx@gmail.com>
2024-09-29 15:50:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -31,3 +31,19 @@ self.addEventListener('activate', event => {
         })
     );
 });
+// Замените 'YOUR_APP_ID' на ваш App ID
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+    OneSignal.init({
+        appId: "13464047-67bc-4b57-9e7d-6e694fbc5a85",
+        // Убедитесь, что вы используете правильные настройки
+        notifyButton: {
+            enable: true,
+        },
+        promptOptions: {
+            slidedown: {
+                enabled: true,
+            },
+        },
+    });
+});
